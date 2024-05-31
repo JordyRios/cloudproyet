eadd304724a0a52c976860ebad5f8dadfa12cf87
JordyRios <119907843+JordyRios@users.noreply.github.com>
2024-05-31 07:33:02 -0600

    Update config.php

--- database/config.php
+++ database/config.php
@@ -5,5 +5,5 @@
 $host       = 'adminuser.mysql.database.azure.com';
 $user       = 'adminuser';
 $password   = 'Chucha@1234567';
-$db_name    = 'proyect';
+$db_name    = 'PRODUCTS';
 // $sslcert    = "ssl/DigiCertGlobalRootCA.crt.pem";
