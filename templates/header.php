131848558daeb55064028cf3b06cfb642f1d1d12
JordyRios <119907843+JordyRios@users.noreply.github.com>
2024-05-30 21:29:07 -0600

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -5,13 +5,17 @@
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Varias cosas/title>
+	<title>Varias cosas</title>
 	<link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
 	<div class="center-align"> 
-		<h1>Igresar datos</h1>
+		<h1>Ingresar datos</h1>
 		<p>Computo en la nube</p>
 		<p>Ing. Sistemas Computacionales</p>
 		<br>
 	</div>
+</body>
+
+</html>
